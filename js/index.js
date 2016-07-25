cf3a908464ff652e0d94fe58610d1ee2f85e0914
DawHawk808 <hawkeye808@Dawsons-MacBook-Air.local>
2016-07-24 20:29:58 -1000

    aa

--- js/index.js
+++ js/index.js
@@ -7,21 +7,42 @@ var Monsters = ["<img src='../assets/ogre.png> Ogre",
 
 var PlayerHP = 1;
 
-var Difattacks = ["Threw a rock", "Threw a stick"]
+var Difattacks = [" Threw a rock", " Threw a stick", " Threw a thorn", " Threw a piece of wood"]
 
 var Sword;
 var Slingshot;
 
-var armor = 175;
-var badarmor;
-var morearmor;
+var Armor = 125;
+var Badarmor;
+var Morearmor;
 
-var atkpower;
-var monsteratk;
+var Atkpower;
+var Monsteratk;
+
+var Dodge;
 
 function newmonster() {
-	var badguys = Math.floor(Math.random()*Monsters.length);
-	var badarmor = Math.floor(Math.random()*200);
-	console.log("A "+ Monsters[badguys] +" Spawned!")
-	document.getElementById('monsterresult').innerhtml = "A " + Monsters[badguys] + " Spawned!"
+	var Badguys = Math.floor(Math.random()*Monsters.length);
+	Badarmor = Math.floor(Math.random()*100);
+	console.log("A "+ Monsters[Badguys] +" Spawned!");
+	document.getElementById('monsterresult').innerhtml = "A " + Monsters[Badguys] + " Spawned!";
+	document.getElementById('monsterarea').innerhtml = Monsters[Badguys];
+};
+
+function dodgehit() {
+	var Miss = Math.floor(Math.random()*11);
+if (var Dodge > 5) {
+	console.log("monster missed")
+	document.getElementById('playerresult').innerhtml = "You dodge the monsters attack";
+} else {
+	document.getElementById('playerresult').innerhtml = "The monster got to you before you dodged";
 }
+
+};
+
+function attack() {
+	var Multiatk = Math.floor(Math.random()*Difattacks.length);
+	Atkpower = Math.floor(Math.random()*11);
+	Monsteratk = Math.floor(Math.random()*6);
+	if (var PlayerHP = 0) {}
+};
